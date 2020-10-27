5b2ddfc30c6f50c87a44e7057fb5975270b1d462
jingyaopu <jingyaopu@gmail.com>
2020-10-26 20:28:29 -0400

    Updates

---
+++ precache-manifest.cb98422bc8999e756178f23eb371668f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d2b9972785dc1ebc0db08ad11df8d8",
+    "url": "/github-api/index.html"
+  },
+  {
+    "revision": "993ae5c89990c7499367",
+    "url": "/github-api/static/css/main.4fd06f95.chunk.css"
+  },
+  {
+    "revision": "0f8fae1f4859d9fe1595",
+    "url": "/github-api/static/js/2.0cbd7d4d.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/github-api/static/js/2.0cbd7d4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993ae5c89990c7499367",
+    "url": "/github-api/static/js/main.ae297a6c.chunk.js"
+  },
+  {
+    "revision": "08d2431d44931acc0d0e",
+    "url": "/github-api/static/js/runtime-main.57c7f886.js"
+  }
+]);
